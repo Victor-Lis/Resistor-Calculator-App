f3b4c535dc967fac1e767fe02ebc35941a10e826
Victor-Lis <victorlisbronzo1@gmail.com>
2024-03-03 14:16:44 -0300

    v1.07
    - Tipando cores;
    - Criando array com as cores.

---
+++ src/@types/cor.ts
@@ -0,0 +1,6 @@
+export type CorType = {
+    title: string,
+    hex: string,
+    valor: string,
+    multiplicador: number
+}
---
+++ src/utils/cores.ts
@@ -0,0 +1,64 @@
+import { CorType } from "../@types/cor";
+
+export const cores: CorType[] = [
+  {
+    title: "Preto",
+    hex: "#000",
+    valor: "0",
+    multiplicador: 1,
+  },
+  {
+    title: "Marrom",
+    hex: "#6b350c",
+    valor: "1",
+    multiplicador: 10,
+  },
+  {
+    title: "Vermelho",
+    hex: "#ff0000",
+    valor: "2",
+    multiplicador: 100,
+  },
+  {
+    title: "Laranja",
+    hex: "#e3781c",
+    valor: "3",
+    multiplicador: 1000,
+  },
+  {
+    title: "Amarelo",
+    hex: "#f7ee0e",
+    valor: "4",
+    multiplicador: 10000,
+  },
+  {
+    title: "Verde",
+    hex: "#8fcc4c",
+    valor: "5",
+    multiplicador: 100000,
+  },
+  {
+    title: "Azul",
+    hex: "#04abed",
+    valor: "6",
+    multiplicador: 1000000,
+  },
+  {
+    title: "Roxo",
+    hex: "#6b2fa1",
+    valor: "7",
+    multiplicador: 10000000,
+  },
+  {
+    title: "Cinza",
+    hex: "#a2a39d",
+    valor: "8",
+    multiplicador: 100000000,
+  },
+  {
+    title: "Branco",
+    hex: "#f7f7f7",
+    valor: "9",
+    multiplicador: 1000000000,
+  },
+];
