2af3a97e27feadd59f8fbca074eac52b013ad1e3
Victor-Lis <victorlisbronzo1@gmail.com>
2024-03-04 10:22:40 -0300

    v1.16
    - Construindo tela ColorToNum

--- src/Pages/ColorToNum/index.tsx
+++ src/Pages/ColorToNum/index.tsx
@@ -1,7 +1,24 @@
-import { View } from 'react-native';
+import React, { useState } from "react";
+import { CorType } from "../../@types/cor";
+import { Container, Form } from "./styles";
+import Resistor from "../../components/Resistor";
+import Input from "./Components/Input";
 
-export default function ColorToNum() {
- return (
-   <View/>
+export default function NumToColor() {
+  const [color1, setColor1] = useState<CorType | null>();
+  const [color2, setColor2] = useState<CorType | null>();
+  const [color3, setColor3] = useState<CorType | null>();
+  const [num, setNum] = useState<string | null>()
+ 
+  return (
+    <Container>
+      <Resistor color1={color1?.hex} color2={color2?.hex} color3={color3?.hex} />
+      <Form>
+        <Input 
+          title="Número de ohms" setItem={setNum}
+          setColor1={setColor1} setColor2={setColor2} setColor3={setColor3}
+        />
+      </Form>
+    </Container>
   );
 }
---
+++ src/Pages/ColorToNum/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+
+export const Container = styled.SafeAreaView`
+    background-color: #252525;
+    flex: 1;
+    padding: 50px 0;
+`
+
+export const Form = styled.View`
+    padding: 50px 0;
+`
