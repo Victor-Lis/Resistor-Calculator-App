66c753cd13f5c0903ceeb2e0877ea4fb93b43b63
Victor-Lis <victorlisbronzo1@gmail.com>
2024-03-04 10:48:46 -0300

    v1.18
    - Ajustando input

--- src/Pages/ColorToNum/Components/Input/index.tsx
+++ src/Pages/ColorToNum/Components/Input/index.tsx
@@ -6,17 +6,18 @@ import { ohmsToColor } from "../../../../utils/ohmsToColor";
 
 type Props = {
   title: string;
-  setItem: React.Dispatch<React.SetStateAction<string | null | undefined>>;
   setColor1: React.Dispatch<React.SetStateAction<CorType | null | undefined>>;
   setColor2: React.Dispatch<React.SetStateAction<CorType | null | undefined>>;
   setColor3: React.Dispatch<React.SetStateAction<CorType | null | undefined>>;
 };
 
-export default function InputComponent({ title, setItem, setColor1, setColor2, setColor3 }: Props) {
+export default function InputComponent({ title, setColor1, setColor2, setColor3 }: Props) {
     return (
       <InputBox>
         <InputText>{title}</InputText>
-        <Input keyboardType="numeric" placeholder="30Ω" onChangeText={(e) => ohmsToColor({value: parseInt(e), setColor1: setColor1, setColor2: setColor2, setColor3: setColor3})}/>
+        <Input keyboardType="numeric" placeholder="30Ω" onChangeText={(e) => {
+          ohmsToColor({value: parseInt(e), setColor1: setColor1, setColor2: setColor2, setColor3: setColor3})
+        }}/>
       </InputBox>
     );
 }
