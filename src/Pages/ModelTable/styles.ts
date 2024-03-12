ca381b9d390510847e7903b30ea8f0f63948a7b9
Victor-Lis <victorlisbronzo1@gmail.com>
2024-03-11 21:27:59 -0300

    v1.37
    - Estilizando tela ModelTable

---
+++ src/Pages/ModelTable/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Container = styled.SafeAreaView`
+    background-color: #252525;
+    flex: 1;
+    padding: 50px 0;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Image = styled.Image`
+    width: 90%;
+    height: 150px;
+`
